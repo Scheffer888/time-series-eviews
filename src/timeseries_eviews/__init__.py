d5ce03b5dc55fd3c02ad6985d3d38b4090f2aee9
Scheffer888 <eduardo.b.scheffer@gmail.com>
2025-02-27 12:07:21 -0600

    Updated __init__

---
+++ src/timeseries_eviews/__init__.py
@@ -0,0 +1,8 @@
+"""
+TimeSeries-EViews Package
+
+Copyright (c) 2025 Eduardo Belisario Scheffer
+Licensed under the Apache License, Version 2.0 (the "License");
+You may not use this file except in compliance with the License.
+See the LICENSE file for details.
+"""
